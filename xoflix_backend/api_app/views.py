9b0c563f817c6827ff7e89d1ffaa48651037c5dc
Skull-Face-XOF <fkonstantinidis@nyc.gr>
2026-06-04 18:46:18 -0200

    Add full React frontend

--- xoflix_backend/api_app/views.py
+++ xoflix_backend/api_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
